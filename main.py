325981b8668adda65d0a99f3140cbb2640db3c57
w1z4rdWP <headgenrekty@gmail.com>
2026-04-09 12:50:59 +0400

    Инкапсуляция атрибутов родительского класса

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time, random
 class Quiz:
     def __init__(self, difficulty: int):
         self.difficulty = difficulty
-        self.questions_amount = 0
-        self.correct_answers = 0
-        self.current_question = 0
-        self.correct_answers = 0
-        self.incorrect_answers = 0
+        self._questions_amount = 0
+        self._correct_answers = 0
+        self._current_question = 0
+        self._correct_answers = 0
+        self._incorrect_answers = 0
 
-    def get_quiz(self):
+    def start_quiz(self):
         # Легкая сложность
         if self.difficulty == 0:
             print("Выбери тип теста")
@@ -23,25 +23,28 @@ class Quiz:
 
 class MultiplicationQuiz(Quiz):
     def multiplication_quiz(self, base_num, end_num):
-        self.questions_amount = 10
+        self._questions_amount = 10
         start_time = time.time()
-        while self.current_question < self.questions_amount:
-            self.current_question += 1
+        while self._current_question < self._questions_amount:
+            self._current_question += 1
             num_1 = random.randint(base_num, end_num)
             num_2 = random.randint(base_num, end_num)
-            question = int(input(f"Сколько будет {num_1} * {num_2}?: "))
-            if question == num_1 * num_2:
-                self.correct_answers += 1
+            question = input(f"Сколько будет {num_1} * {num_2}?: ")
+            if question == 'stop':
+                print(f"Прерываем. Твой результат: {self._correct_answers}/{self._incorrect_answers + self._correct_answers}")
+                break
+            if int(question) == num_1 * num_2:
+                self._correct_answers += 1
                 print("Поздравляю! Ответ правильный!")
             else:
-                self.incorrect_answers += 1
+                self._incorrect_answers += 1
                 print(f"Увы :(. \nТвой ответ:{question}\nПравильный ответ: {num_1 * num_2}")
         end_time = time.time()
         res_time = end_time - start_time
         print(
-            f"Правильных ответов: {self.correct_answers}/{self.incorrect_answers + self.correct_answers}\nЗаняло времени: {round(res_time, 2)} секунд.")
+            f"Правильных ответов: {self._correct_answers}/{self._incorrect_answers + self._correct_answers}\nЗаняло времени: {round(res_time, 2)} секунд.")
 
-    def get_quiz(self):
+    def start_quiz(self):
         """
             Метод для проверки навыков умножения двух чисел
         :return: None
@@ -60,25 +63,25 @@ class MultiplicationQuiz(Quiz):
 
 class SummaryQuiz(Quiz):
     def summary_quiz(self, base_num, end_num):
-        self.questions_amount = 10
+        self._questions_amount = 10
         start_time = time.time()
-        while self.current_question < self.questions_amount:
-            self.current_question += 1
+        while self._current_question < self._questions_amount:
+            self._current_question += 1
             num_1 = random.randint(base_num, end_num)
             num_2 = random.randint(base_num, end_num)
             question = int(input(f"Сколько будет {num_1} + {num_2}?: "))
             if question == num_1 + num_2:
-                self.correct_answers += 1
+                self._correct_answers += 1
                 print("Поздравляю! Ответ правильный!")
             else:
-                self.incorrect_answers += 1
+                self._incorrect_answers += 1
                 print(f"Увы :(. \nТвой ответ:{question}\nПравильный ответ: {num_1 + num_2}")
         end_time = time.time()
         res_time = end_time - start_time
         print(
-            f"Правильных ответов: {self.correct_answers}/{self.incorrect_answers + self.correct_answers}\nЗаняло времени: {round(res_time, 2)} секунд.")
+            f"Правильных ответов: {self._correct_answers}/{self._incorrect_answers + self._correct_answers}\nЗаняло времени: {round(res_time, 2)} секунд.")
 
-    def get_quiz(self):
+    def start_quiz(self):
         # Легкая сложность
         if self.difficulty == 0:
             self.summary_quiz(1,100)
@@ -92,7 +95,6 @@ class SummaryQuiz(Quiz):
             self.summary_quiz(100, 1000)
 
 
-
 if __name__ == "__main__":
     summary_choices = ["сложение", "1", 1]
     multiplication_choices = ["умножение", "2", 2]
@@ -100,9 +102,9 @@ if __name__ == "__main__":
     difficulty_choice = int(input("Выбери сложность от 0 до 2: "))
     if quiz_type.lower() in summary_choices:
         quiz = SummaryQuiz(difficulty_choice)
-        quiz.get_quiz()
+        quiz.start_quiz()
     elif quiz_type.lower() in multiplication_choices:
         quiz = MultiplicationQuiz(difficulty_choice)
-        quiz.get_quiz()
+        quiz.start_quiz()
     else:
         print("Заданы неизвестные параметры")
